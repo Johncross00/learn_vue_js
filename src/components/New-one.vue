f2351e9daeece415600d2396c490893fcebb2851
johncross00 <johncross.ktm@gmail.com>
2024-03-14 02:56:24 +0000

    Interpollation

--- src/components/New-one.vue
+++ src/components/New-one.vue
@@ -1,12 +1,21 @@
 <template>
-    <h1>
-        Hello John
-    </h1>
+    <h1>Email: {{ email }}</h1>
+    <h1>Mobile: {{ number }}</h1>
+    <h1>Name: {{ getName() }}</h1>
 </template>
 
 <script>
 export default{
-    name: "PAGES"
+    name: "PAGES",
+    data() {
+        return {
+            email: "abcd@gmail.com",
+            number: "06000000",
+            getName: function(){
+                return "ZOI";
+            }
+        }
+    },
 }
 </script>
 <style scoped>
