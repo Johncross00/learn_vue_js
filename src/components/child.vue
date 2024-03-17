bd7583c65406584adb15beac3d942a2992936c0f
johncross00 <johncross.ktm@gmail.com>
2024-03-17 01:07:18 +0000

    reuse

--- src/components/child.vue
+++ src/components/child.vue
@@ -1,19 +1,58 @@
 <template>
     <div>
-        <h1>Enfant</h1>
+        <h1>Enfant child comp</h1>
         <h2>{{ name }}</h2>
         <h2>{{ userchild.email }}</h2>
         <button v-on:click="passData()" >Call</button>
+        <ul>
+            <li v-for="item in users" :key="item">
+                <!-- {{ item.name }}  -->
+                <newUser :data="item"/>
+
+
+                <!-- &nbsp;{{ item.email }} -->
+            </li>
+        </ul>
+        <hr>
+        <hr>
+        <!-- <newUser :data="users"/> -->
+        
     </div>
 </template>
 
 <script>
+import newUser from "./new-user.vue"
 export default{
     name: "childComp",
     props: {
         name: String,
         userchild: Object,
         passData: Function
+    },
+    data(){
+        return {
+            users: [{
+                name: "Michel",
+                email: "michel@gmail.com",
+            },
+            {
+                name: "John",
+                email: "john@gmail.com",
+            },
+            {
+                name: "Fred",
+                email: "fred@gmail.com",
+            }
+        ]
+        }
+    },
+    components: {
+        newUser
     }
 }
 </script>
+<style scoped>
+li{
+    color: rgb(30, 233, 16);
+}
+</style>
---
+++ src/components/new-user.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Dans le new user Azalaka Pinhou</h1>
+        <h2>Dans le new user: email= {{ data.email }}</h2>
+        <h2>Dans le new user: nom= {{ data.name }}</h2>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "newUser",
+    props: {
+        data: Object,
+    }
+}
+
+</script>
