78a1c59adcacb1412b72b764572eccb413150135
johncross00 <johncross.ktm@gmail.com>
2024-04-08 21:50:19 +0000

    computed

--- src/components/computyProp.vue
+++ src/components/computyProp.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <h1>Computy Property</h1>
+        <span>{{ author.books.length > 0 ? "Oui" : "Non" }}</span> &nbsp;
+        <span>{{ publishedBook }}</span>
     </div>
 </template>
 
@@ -9,8 +11,22 @@
 
 export default{
     name: "computyProp",
-    props: {
-        info: String
+    data() {
+        return {
+            author: {
+                name: "Popo",
+                books: [
+                    "Vue 1",
+                    "Vue 2",
+                    "Vue 3"
+                ]
+            }
+        }
+    },
+    computed: {
+        publishedBook(){
+            return this.author.books.length > 0 ? "Yes" : "No"
+        }
     }
 }
 </script>
