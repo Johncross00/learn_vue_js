c783d236ab0ae5a5799858c65f1b2ab740e21ea6
johncross00 <johncross.ktm@gmail.com>
2024-04-12 08:00:08 +0000

    samples comp

---
+++ src/components/first.vue
@@ -0,0 +1,13 @@
+<template>
+        <h1>First</h1>       
+</template>
+
+<script>
+
+
+export default{
+    name: "firstComp",
+}
+</script>
+<style>
+</style>
---
+++ src/components/second.vue
@@ -0,0 +1,13 @@
+<template>
+        <h1>Second</h1>       
+</template>
+
+<script>
+
+
+export default{
+    name: "secondComp",
+}
+</script>
+<style>
+</style>
